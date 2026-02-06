3173f8c1bc53ebf5ca4089c023c4c4fde70fdbf6
Daniel Juarez <danieljuarez5575@my.whccd.edu>
2026-02-05 22:15:36 -0800

    The for inputs are done and running good

--- COMP003A.LectureActivityy4/Program.cs
+++ COMP003A.LectureActivityy4/Program.cs
@@ -24,6 +24,13 @@ namespace COMP003A.LectureActivityy4
                 attempt++;
             }
             while (attempt <= 3);
+
+            Console.WriteLine("\n");
+
+            for(int i = 1; i <=4; i++)
+            {
+                Console.WriteLine($"Step {i}");
+            }
         }
     }
 }
